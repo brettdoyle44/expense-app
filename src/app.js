e5966ed85fcae3d40bd0465c164dd3f7e83535b7
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2019-09-26 12:56:30 -0400

    Re-organize Redux Files
    - Broke out single Redux file into separate folders
    - Tested everything still works in app.js by importing new files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore()
+
+store.dispatch(
+  addExpense({ description: 'Water Bill', amount: 3000, createdAt: 560000 })
+)
+
+store.dispatch(
+  addExpense({ description: 'Gas Bill', amount: 6000, createdAt: 550000 })
+)
+
+store.dispatch(setTextFilter('water'))
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+
+console.log(visibleExpenses)
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'))
