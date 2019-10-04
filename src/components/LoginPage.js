1f6154a75c3e068abe02fbf8f28f35eec24bd8fd
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2019-10-04 12:33:53 -0400

    Add auth plus auth routes
    - Added firebase login and logout with authentication
    - Created authenticated routes

---
+++ src/components/LoginPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { startLogin } from '../actions/auth'
+
+export const LoginPage = ({ startLogin }) => (
+  <div>
+    <h3>Login Page</h3>
+    <button onClick={startLogin}>Login</button>
+  </div>
+)
+
+const mapDispatchToProps = dispatch => ({
+  startLogin: () => dispatch(startLogin())
+})
+
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(LoginPage)
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import AddExpensePage from '../components/AddExpensePage';
-import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
-import NotFoundPage from '../components/NotFoundPage';
-import Header from '../components/Header';
+import React from 'react'
+import { Router, Route, Switch } from 'react-router-dom'
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
+import AddExpensePage from '../components/AddExpensePage'
+import EditExpensePage from '../components/EditExpensePage'
+import HelpPage from '../components/HelpPage'
+import NotFoundPage from '../components/NotFoundPage'
+import Header from '../components/Header'
+import LoginPage from '../components/LoginPage'
+import createHistory from 'history/createBrowserHistory'
+import AuthenticatedRoute from './AuthenticatedRoute'
+
+export const history = createHistory()
 
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <div>
-      <Header />
       <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
+        <Route path="/" component={LoginPage} exact={true} />
+        <AuthenticatedRoute
+          path="/dashboard"
+          component={ExpenseDashboardPage}
+        />
+        <AuthenticatedRoute path="/create" component={AddExpensePage} />
+        <AuthenticatedRoute path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
-  </BrowserRouter>
-);
+  </Router>
+)
 
-export default AppRouter;
+export default AppRouter
