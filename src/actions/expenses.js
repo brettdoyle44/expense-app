5c8f0cba9661b2310f58d0b8fc49100d1f31b439
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2019-10-03 23:58:48 -0400

    Add ability to edit expense via the db

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,4 +1,4 @@
-import uuid from 'uuid'
+// import uuid from 'uuid'
 import database from '../firebase/firebase'
 
 // ADD_EXPENSE
@@ -53,6 +53,17 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 })
+
+export const startEditExpense = (id, updates) => {
+  return dispatch => {
+    return database
+      .ref(`expenses/${id}`)
+      .update(updates)
+      .then(() => {
+        dispatch(editExpense(id, updates))
+      })
+  }
+}
 
 // SET_EXPENSES
 
